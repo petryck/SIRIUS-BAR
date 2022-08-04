5a54634d01eaee0a95a804c58feea7106b9c3a39
Petryck WIlliam <petryck@i9devgroup.com>
2022-08-04 10:44:36 -0300

    .
    .

--- srv/index.js
+++ srv/index.js
@@ -1,8 +1,41 @@
-const { app,BrowserWindow, Notification, ipcMain } = require("electron");
+const { app,BrowserWindow, Notification, ipcMain, autoUpdater, dialog } = require("electron");
 const path = require("path");
 const { setTimeout } = require("timers/promises");
 const { modulos } = require("./query/consultas");
-require('update-electron-app')()
+// require('update-electron-app')()
+
+const server = 'https://sirius-bar-e4or.vercel.app/'
+const url = `${server}/update/${process.platform}/${app.getVersion()}`
+
+autoUpdater.setFeedURL({ url })
+
+setInterval(() => {
+  autoUpdater.checkForUpdates()
+}, 60000)
+
+
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Reiniciar', 'Mais Tarde'],
+    title: 'Atualização Disponivel',
+    message: process.platform === 'win32' ? releaseNotes : releaseName,
+    detail:
+      'Uma nova versão foi Baixada. Reinicie o Aplicativo para Instalar as Atualizações.',
+  }
+
+  dialog.showMessageBox(dialogOpts).then((returnValue) => {
+    if (returnValue.response === 0) autoUpdater.quitAndInstall()
+  })
+})
+
+autoUpdater.on('error', (message) => {
+  console.error('Ocorreu um erro ao atualizar o aplicativo')
+  console.error(message)
+})
+
+
+
 
 let mainWindow;
 let menu_pop;
